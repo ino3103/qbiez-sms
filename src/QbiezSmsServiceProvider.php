f1a6a39891285267d78e751d73efeab000c1b229
Ino Moses <inomosjac@gmail.com>
2025-04-27 20:46:04 +0300

    Fixed config paths and service provider implementation

--- src/QbiezSmsServiceProvider.php
+++ src/QbiezSmsServiceProvider.php
@@ -3,31 +3,57 @@
 namespace Qsms\QbiezSms;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
 
-class QbiezSmsServiceProvider extends ServiceProvider
+class QbiezSmsServiceProvider extends ServiceProvider implements DeferrableProvider
 {
-    /**
-     * Register services.
-     */
     public function register()
     {
-        // Automatically merge package config
-        $this->mergeConfigFrom(
-            __DIR__ . '/../config/qsms.php',
-            'qsms'
-        );
+        $configPath = __DIR__ . '/../config/qsms.php'; // Fixed path
+
+        if (file_exists($configPath)) {
+            $this->mergeConfigFrom($configPath, 'qsms');
+        } else {
+            $this->app['log']->warning('Qbiez SMS config file not found at: ' . $configPath);
+        }
+
+        $this->app->singleton('qsms', function ($app) {
+            return new SendSMS();
+        });
+
+        if (!class_exists('Qsms')) {
+            class_alias(Facades\Qsms::class, 'Qsms');
+        }
     }
 
-    /**
-     * Bootstrap services.
-     */
     public function boot()
     {
-        // Publish config file
         if ($this->app->runningInConsole()) {
-            $this->publishes([
-                __DIR__ . '/../config/qsms.php' => config_path('qsms.php'),
-            ], 'qsms-config');
+            $this->publishConfig();
+            $this->registerCommands();
         }
+    }
+
+    protected function publishConfig()
+    {
+        $configPath = __DIR__ . '/../config/qsms.php'; // Fixed path
+
+        $this->publishes([
+            $configPath => config_path('qsms.php'),
+        ], ['qsms-config', 'config']);
+    }
+
+    protected function registerCommands()
+    {
+        if (class_exists(Console\CleanLogsCommand::class)) { // Added namespace
+            $this->commands([
+                Console\CleanLogsCommand::class
+            ]);
+        }
+    }
+
+    public function provides()
+    {
+        return ['qsms', SendSMS::class];
     }
 }
